267d3a2e0a5cc6942f9c9208e32709acc92518db
One Roof Web <athar.career@gmail.com>
2026-01-10 23:55:05 +0530

    Initialize files

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    function updateLightbox(element) {
+        var imgSrc = element.src;
+        var lightboxImg = document.getElementById('lightboxImage');
+        lightboxImg.src = imgSrc;
+    }
